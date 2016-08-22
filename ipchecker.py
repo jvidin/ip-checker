d3edd07b36f9ce1b522fae038482e87642db0483
Jorge Vidinha <jorge.vidinha@here.com>
2016-08-22 14:03:03 +0100

    adding dropbox config.py to isolate credentials from vcs

--- ipchecker.py
+++ ipchecker.py
@@ -6,12 +6,16 @@ import ipgetter
 import platform
 import json
 import logging
+import os
+import config
 
-app_key = '6u3nzsyr5kj6isq'
-app_secret = 'vdv5u7tk04tdqan'
-token = 'uMFjOVN-lEIAAAAAAAAB0OIrQuGRX2IfeWdrhFpfRlcWs37a1FcGr3e3NMu9OQoE'
+
+app_key = config.keys['app_key']
+app_secret = config.keys['app_secret']
+token = config.keys['token']
 
 # python 3.4 version
+
 
 def get_logger():
     logger = logging.getLogger("logging_ipchecker")
@@ -25,22 +29,21 @@ def get_logger():
     logger.addHandler(fh)
     return logger
 
+
 def read_ip_mem():
-    with open('ip_in_memory.json', 'r') as ip:
+    with open(os.path.join(os.getcwd(), 'ip_in_memory.json'), 'r') as ip:
         ip_memory = json.load(ip)
-        #print('ip_in_memory is ->{}'.format(ip_memory['ip']))
         logger.debug('ip_in_memory is ->{}'.format(ip_memory['ip']))
         return ip_memory
 
 
 def compare_ip(ip_memory, ip):
     if ip_memory['ip'] != ip:
-        #print('ip in memory changed to->{}'.format(ip_memory))
         logger.debug('ip_in_memory changed to->{}'.format(ip))
         write_ip_mem(ip)
         return 1
     else:
-        #print('ip_checked->{} same as  ip_in_memory->{}'.format(ip_check, ip_memory))
+        logger.debug('ip_checked-> {} same as  ip_in_memory-> {}'.format(ip, ip_memory['ip']))
         return 0
 
 
@@ -52,7 +55,6 @@ def write_ip_mem(ip):
 
 def ip_check():
     ip = ipgetter.myip()
-    #print('ip_checked is ->{}'.format(ip))
     logger.debug('ip_checked is ->{}'.format(ip))
     return ip
 
@@ -67,18 +69,14 @@ def ip_in_memory(ip):
 def ip_to_dropbox(ip):
     with open('myip_{}.csv'.format(platform.system()), 'a') as f:
         f.write(str(datetime.datetime.now()) + '|' + ip + '|' + platform.python_version() + '|' + platform.system() + '|' + '\n')
-        #print('actual ip ', ip_check())
         logger.debug('actual ip ->{}'.format(ip))
     try:
         client = dropbox.client.DropboxClient(token)
-        #print('linked account: ', client.account_info())
         dropFile = open('myip_{}.csv'.format(platform.system()), 'rb')
         dropFileResponse = client.put_file('myip_{}.csv'.format(platform.system()), dropFile, overwrite=True)
-        #print('uploaded', dropFileResponse)
-        logger.debug('uploaded', dropFileResponse)
+        logger.debug('uploaded {}'.format(dropFileResponse))
 
     except Exception as e:
-        #print(e)
         with open('myip_error_log_{}.csv'.format(platform.system()), 'a') as ferr:
             ferr.write(str(e))
 
